0ec7fd86a5dc20c0be0b9012ee60a86139b130f0
DeschrevelBert <71965636+DeschrevelBert@users.noreply.github.com>
2023-06-19 09:58:32 +0200

    Alert with page refresh added

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -51,12 +51,13 @@ export default function Searchbar({setResults} : any) {
       fetchSearchData(value)
     }
     return(
-        <div className="relative mt-2">
-            <BiSearchAlt className="absolute top-1/2 scale-150 ml-4 -translate-y-1/2 text-darkblue" />
-            <input type="text" value={input} onChange={(e) => handleChange(e.target.value)} 
-            className="bg-lightblue text-md pl-12 p-4 rounded w-full transition-all ease-in-out duration-200 max-w-lg
-            focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-darkblue focus:rounded
-            hover:outline-none hover:ring-2 hover:ring-offset-4 hover:ring-darkblue hover:rounded" />
-        </div>
+      <div className="relative mt-2">
+          <BiSearchAlt className="absolute top-1/2 scale-150 ml-4 -translate-y-1/2 text-darkblue" />
+          <input type="text" value={input} onChange={(e) => handleChange(e.target.value)} 
+          onSubmit={(e) => e.preventDefault()} // Prevents page refresh on enter
+          className="bg-lightblue text-md pl-12 p-4 rounded w-full transition-all ease-in-out duration-200 max-w-lg
+          focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-darkblue focus:rounded
+          hover:outline-none hover:ring-2 hover:ring-offset-4 hover:ring-darkblue hover:rounded" />
+      </div>
     )
 }

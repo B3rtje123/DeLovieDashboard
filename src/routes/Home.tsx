ce564afcd03973f9bd54dc9d85c13f55937bd913
DeschrevelBert <71965636+DeschrevelBert@users.noreply.github.com>
2023-06-19 10:00:45 +0200

    width on mobile changed

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
       <main className="font-pop max-h-screen lg:overflow-hidden">
       <LogoBar />
-      <div className="max-w-sm md:max-w-2xl lg:max-w-4xl xl:max-w-7xl flex flex-col mx-auto">
+      <div className="max-w-xs md:max-w-2xl lg:max-w-4xl xl:max-w-7xl flex flex-col mx-auto">
         <h2>Dashboard</h2>
         <h1 className="text-4xl font-semibold mt-7">Dashboard</h1>
         <Searchbar setResults={setResults}/>
